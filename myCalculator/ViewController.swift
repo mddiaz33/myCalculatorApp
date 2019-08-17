dd30687b60345b4ccb3cbf56aaee49c5319e9c10
Maria Diaz <mddiaz33@neiu.edu>
2019-08-16 20:46:33 -0400

    Initial Commit

---
+++ myCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myCalculator
+//
+//  Created by Maria Diaz on 8/16/19.
+//  Copyright © 2019 AppDev_MD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
